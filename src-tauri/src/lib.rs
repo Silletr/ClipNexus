bfa4186ec5fb0c5a7712356f625284bac1b2ae07
Silletr <silletrMain@gmail.com>
2026-04-19 00:08:53 +0300

    [CHANGED FILE/DIR: package-lock.json, package.json, src-tauri/*, src/app.html | NEW FILE/DIR: src-tauri/capabilities/desktop.json] [Type: Returned static/*] Returned static/* (as turned out it's important folder xD). Also trying to make 'for_something_else' function work. Nothing good :- [TIMESTAMP: 19/04 | 0:08 UTC+2]

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,21 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+/*
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+*/
 
 #[tauri::command]
 fn for_something_else() -> String {
-    "This is something happend, but idk what exactly happend so yes".to_string()
+    format!("Something is happenin'")
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, for_something_else])
+        .invoke_handler(tauri::generate_handler![for_something_else])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
